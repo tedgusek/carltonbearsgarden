d7bd9c579c804902cb06724a0a1f9dff64ecb03c
Ted Gusek <theohgusek@gmail.com>
2024-03-13 22:14:37 -0400

    edited name text size on small window

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-green-700">
       <div className="z-10 max-w-5xl w-full font-mono text-sm flex items-center justify-center">
-        <p className="fixed left-0 top-0 flex items-center w-full justify-start border-b bg-gradient-to-r from-green-300 h-24 text-5xl text-black font-extrabold md:max-w-64 md:h-64 px-10">
+        <p className="fixed left-0 top-0 flex items-center w-full justify-start border-b bg-gradient-to-r from-green-300 h-24 md:text-5xl text-black font-extrabold md:max-w-64 md:h-64 px-10 text-3xl">
           Carlton Bears Garden
         </p>
         {/* <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
