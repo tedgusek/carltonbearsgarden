a1e97a638a0d5e8c09a0f041104bfa2f21eca2fe
Ted Gusek <theohgusek@gmail.com>
2024-04-27 16:54:43 -0700

    cleaning up styles of content pages and welcome modal

--- src/components/modals/WelcomeModal.tsx
+++ src/components/modals/WelcomeModal.tsx
@@ -1,41 +1,30 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { WelcomeModalProps } from '@/app/types/interface';
-// import CharCard from '../charCard/CharCard';
-// import { BattleModalProps } from '@/app/types/interface';
-// import { useState } from 'react';
 
 const WelcomeModal: React.FC<WelcomeModalProps> = ({ onClose }) => {
-  //   const [isModalOpen, setIsModalOpen] = useState<boolean>(true);
-
-  //   if (!characters[0] || !characters[1]) return;
-
-  //   const doNothing = () => {
-  // console.log(
-  //   'this is a placeholder to fill in a param so that I could reuse the CharCard component'
-  // );
-  //   };
-
   const handleCloseModal = () => {
     onClose();
-    //   battleOnClick();
   };
 
   return (
     <div className='fixed inset-0 flex flex-col items-center justify-center bg-yellow-100 bg-opacity-50 z-50 '>
-      <div className='max-w-md w-full p-4 bg-yellow-50 rounded-xl shadow-lg flex flex-col items-center justify-center border-gray-500 border-4 shadow-gray-500'>
-        <h2 className='text-xl font-bold mb-2 text-black'>
-          Welcome To Carlton Bears Garden
+      <div className='max-w-md w-full p-4 bg-yellow-50 rounded-xl shadow-lg flex flex-col  border-gray-500 border-4 shadow-green-100'>
+        <h2 className='text-xl font-bold mb-2 text-black '>
+          Welcome To Carlton Bears Garden (Website!)
         </h2>
-        <div className='flex flex-row items-center justify-center m-4 text-black'>
-          Filler text goes here, talk about the garden, all are welcome. Please
-          explore our site to see how you can get involved.
+        <div className='flex flex-row items-stretch justify-center m-4 text-black'>
+          We are one of over 550 community gardens in NYC. You can find info
+          here on our garden&apos;s history, info on how to join, and for
+          current members to pay suggested dues.
         </div>
-        <button
-          className='flex flex-col items-center bottom-1 rounded-full px-4 bg-green-600 bg-opacity-70 hover:bg-opacity-100'
-          onClick={handleCloseModal}
-        >
-          Close
-        </button>
+        <div className='flex flex-col items-center'>
+          <button
+            className='flex flex-col items-center bottom-1 rounded-full px-4 bg-green-600 bg-opacity-70 hover:bg-opacity-100 w-60'
+            onClick={handleCloseModal}
+          >
+            Enter
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/content/SuggestedDues.tsx
+++ src/content/SuggestedDues.tsx
@@ -7,9 +7,15 @@ interface SuggestedDuesPageProps {
 
 const SuggestedDuesPage: React.FC<SuggestedDuesPageProps> = ({ content }) => {
   return (
-    <div className='h-full w-full text-black flex flex-col items-center'>
-      <p className='font-bold'> Suggested Dues </p>
-      <DonateButton />
+    // <div className='h-full w-full text-black flex flex-col items-center'>
+    <div className='h-96 w-full  text-black flex flex-col items-stretch'>
+      <h5 style={{ fontSize: '1.5rem' }} className='font-bold'>
+        Suggested Dues
+      </h5>
+      {/* <p className='font-bold'> Suggested Dues </p> */}
+      <div className='flex flex-col items-center'>
+        <DonateButton />
+      </div>
       <p className='mt-6'>
         Dues are strongly suggested to help maintain the integrity of our space.
         They are collected once a seaon, they help fund the general maintenance
