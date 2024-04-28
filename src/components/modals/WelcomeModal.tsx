b6266c26e6f101afbca477a7a7960e8f2b0b85f0
Ted Gusek <theohgusek@gmail.com>
2024-04-27 17:07:16 -0700

    cleaned up modal styling

--- src/components/modals/WelcomeModal.tsx
+++ src/components/modals/WelcomeModal.tsx
@@ -9,17 +9,17 @@ const WelcomeModal: React.FC<WelcomeModalProps> = ({ onClose }) => {
   return (
     <div className='fixed inset-0 flex flex-col items-center justify-center bg-yellow-100 bg-opacity-50 z-50 '>
       <div className='max-w-md w-full p-4 bg-yellow-50 rounded-xl shadow-lg flex flex-col  border-gray-500 border-4 shadow-green-100'>
-        <h2 className='text-xl font-bold mb-2 text-black '>
+        <h2 className='text-xl font-bold mb-2 text-black flex flex-col items-center text-center'>
           Welcome To Carlton Bears Garden (Website!)
         </h2>
-        <div className='flex flex-row items-stretch justify-center m-4 text-black'>
+        <div className='flex flex-row items-stretch justify-center m-4 text-black text-center'>
           We are one of over 550 community gardens in NYC. You can find info
           here on our garden&apos;s history, info on how to join, and for
           current members to pay suggested dues.
         </div>
         <div className='flex flex-col items-center'>
           <button
-            className='flex flex-col items-center bottom-1 rounded-full px-4 bg-green-600 bg-opacity-70 hover:bg-opacity-100 w-60'
+            className='flex flex-col items-center bottom-1 rounded-full px-4 bg-green-600 bg-opacity-70 hover:bg-opacity-100 max-w-60'
             onClick={handleCloseModal}
           >
             Enter
