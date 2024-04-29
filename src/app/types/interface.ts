05c8515a439a558eaba78480d203925988b5766b
Ted Gusek <theohgusek@gmail.com>
2024-04-29 17:05:46 -0400

    added email component and minor cleanup

--- src/app/types/interface.ts
+++ src/app/types/interface.ts
@@ -1,16 +1,3 @@
 export interface WelcomeModalProps {
   onClose: () => void;
 }
-
-// export interface GmailIconProps {
-//     body: string;
-// }
-
-export interface GmailIconProps {
-  //   initialBody?: string;
-  isFormOpen: boolean;
-  handleForm: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  handleChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
-  setBody: (arg0: string) => void;
-  body: string;
-}
--- src/components/GmailIcon.tsx
+++ src/components/GmailIcon.tsx
@@ -1,83 +1,17 @@
-// import React, { FC } from 'react';
-// import { GmailIconProps } from '@/app/types/interface';
-
-// // interface GmailIconProps {
-// //   body: string;
-// // }
-
-// const GmailIcon: FC<GmailIconProps> = ({ body }) => {
-//   const email = 'carltonbearsgarden@gmail.com';
-//   const subject = 'Email%20from%20the%20Website'; // You can change this to your desired subject
-
-//   const mailtoLink = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(
-//     body
-//   )}`;
-
-//   return (
-//     <a href={mailtoLink} target='_blank' rel='noopener noreferrer'>
-//       <svg
-//         xmlns='http://www.w3.org/2000/svg'
-//         aria-label='Gmail'
-//         role='img'
-//         viewBox='0 0 24 24'
-//         fill='none'
-//         stroke='currentColor'
-//         strokeWidth='2'
-//         strokeLinecap='round'
-//         strokeLinejoin='round'
-//         className='z-40 w-12 fixed bottom-20 left-4 rounded-full bg-red-500'
-//       >
-//         <path d='M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm5.625 5.709L12 13.118l-5.625-5.409h11.25zm-11.25 8.582L12 10.882l3.625 4.41H5.375zm1.278-9.746L12 11.447l5.347-4.402 1.573 1.201-6.92 5.537-6.92-5.537 1.573-1.201z'></path>
-//       </svg>
-//     </a>
-//   );
-// };
-
-// export default GmailIcon;
 import React, { FC, useState } from 'react';
-import { GmailIconProps } from '@/app/types/interface';
 
-// interface GmailIconProps {
-//   initialBody?: string;
-// }
-
-const GmailIcon: FC<GmailIconProps> = ({
-  isFormOpen,
-  handleForm,
-  handleChange,
-  setBody,
-  body,
-}) => {
-  // const [isFormOpen, setIsFormOpen] = useState(false);
-  // const [body, setBody] = useState(initialBody);
-
-  // const openForm = () => {
-  //   setIsFormOpen(true);
-  // };
-
-  // const closeForm = () => {
-  //   setIsFormOpen(false);
-  // };
-
-  // const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-  //   setBody(event.target.value);
-  // };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+const GmailIcon: FC = () => {
+  const handleSubmit = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     const email = 'carltonbearsgarden@gmail.com';
-    const subject = 'Subject%20of%20the%20email'; // You can change this to your desired subject
-    const mailtoLink = `mailto:${email}?subject=${subject}&body=${encodeURIComponent(
-      body
-    )}`;
+    const subject = 'Inquiry%20about%20the%20Garden%20from%20the%20website'; // You can change this to your desired subject
+    const mailtoLink = `mailto:${email}?subject=${subject}`;
     window.location.href = mailtoLink;
-    // Optionally, you can also close the form here if needed
-    handleForm();
   };
 
   return (
     <div>
-      <button onClick={handleForm}>
+      <button onClick={handleSubmit}>
         <svg
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 24 24'
@@ -96,34 +30,6 @@ const GmailIcon: FC<GmailIconProps> = ({
         </svg>
         ;
       </button>
-      {isFormOpen && (
-        <form
-          onSubmit={handleSubmit}
-          className='bg-yellow-100 rounded-lg border-4 border-green-100 h-60 flex flex-col items-center'
-        >
-          <textarea
-            value={body}
-            onChange={handleChange}
-            placeholder='Type your message here...'
-            rows={15}
-            cols={30}
-            className='m-4 p-2 z-50 text-black'
-          />
-          <button
-            type='submit'
-            className='text-black rounded-xl bg-green-400 px-4'
-          >
-            Send
-          </button>
-          <button
-            type='button'
-            className='text-black rounded-xl bg-red-500 px-4 m-2'
-            onClick={handleForm}
-          >
-            Cancel
-          </button>
-        </form>
-      )}
     </div>
   );
 };
